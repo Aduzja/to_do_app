940e5795134237602376caba074f5972b84bb74a
Adrianna Gerek <aduzja@gmail.com>
2022-11-22 19:05:27 +0100

    Create a task page

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class Constants {
   static const Color colorPink = Color(0xFFD62598);
   static const Color colorBlue = Color(0xFF009ACE);
+  static const Color colorMustard = Color(0xFFF6BE00);
 }
