66a72df4c021cf1cc7a55e7dcb549558c6dd9ffa
Adrianna Gerek <aduzja@gmail.com>
2022-11-22 19:32:20 +0100

    Install FirebaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:to_do_app/helpers/size_helper.dart';
@@ -23,8 +24,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SecondPage(),
+      home: const LoginPage(),
     );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (user == null) {
+            return const Scaffold(
+              body: Center(
+                child: Text('You are not logged in'),
+              ),
+            );
+          }
+          return Scaffold(
+            body: Center(
+              child: Text('You are logged as ${user.email}'),
+            ),
+          );
+        });
   }
 }
 
