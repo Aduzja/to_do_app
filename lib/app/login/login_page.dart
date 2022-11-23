37e73814e59b608eb61123360c330ca89c8bf5fc
Adrianna Gerek <aduzja@gmail.com>
2022-11-23 06:57:53 +0100

    Add login functionality

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,11 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:to_do_app/app/tasks/second_page.dart';
+import 'package:to_do_app/helpers/size_helper.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     Key? key,
   }) : super(key: key);
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +19,69 @@ class LoginPage extends StatelessWidget {
         builder: (context, snapshot) {
           final user = snapshot.data;
           if (user == null) {
-            return const Scaffold(
+            return Scaffold(
               body: Center(
-                child: Text('You are not logged in'),
+                child: Padding(
+                  padding: EdgeInsets.all(
+                    SizeHelper.getSizeFromPx(context, 55),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Log in',
+                        style: GoogleFonts.poppins(
+                          fontSize: SizeHelper.getSizeFromPx(context, 55),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeHelper.getSizeFromPx(context, 55),
+                      ),
+                      TextField(
+                        controller: emailController,
+                        decoration: const InputDecoration(hintText: 'E-mail'),
+                      ),
+                      TextField(
+                        controller: passwordController,
+                        decoration: const InputDecoration(hintText: 'Password'),
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: SizeHelper.getSizeFromPx(context, 55),
+                      ),
+                      Text(
+                        'You are not logged in',
+                        style: GoogleFonts.poppins(
+                          fontSize: SizeHelper.getSizeFromPx(context, 35),
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeHelper.getSizeFromPx(context, 55),
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          try {
+                            await FirebaseAuth.instance
+                                .signInWithEmailAndPassword(
+                                    email: emailController.text,
+                                    password: passwordController.text);
+                          } catch (error) {
+                            print(error);
+                          }
+                        },
+                        child: Text(
+                          'Log in',
+                          style: GoogleFonts.poppins(
+                            fontSize: SizeHelper.getSizeFromPx(context, 45),
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             );
           }
